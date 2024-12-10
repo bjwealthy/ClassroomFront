893238e337230425293b0c4a13ce4e22fd9c138e
Wewrangler <wewranglers@gmail.com>
2024-12-10 15:26:35 +0100

    disable commented

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import {disableReactDevTools} from 'fvilers/disable-react-devtools';
+//import {disableReactDevTools} from 'fvilers/disable-react-devtools';
 
 //if (process.env.NODE_ENV === 'production') disableReactDevTools()
 
